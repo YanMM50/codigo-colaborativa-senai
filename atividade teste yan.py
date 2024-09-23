dd1508b3602d725212cced3fc1b287b895fb16c3
YanMM50 <yanmendes50@gmail.com>
2024-09-23 18:32:21 +0000

    Atividade teste random

---
+++ atividade teste yan.py
@@ -0,0 +1,14 @@
+import os
+os.system("cls || clear")
+
+import random
+
+for i in range (6):
+    numero = random.randint(0,6)
+
+
+print(f"Número sorteado: {numero}")
+
+
+if numero == 5:
+    print("Ola!")
